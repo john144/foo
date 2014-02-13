bb37e12327f3b6f07f968cdeb03c9290b1bba0f9
john144 <john144@gmail.com>
2014-02-13 15:21:48 -0600

    Initial commit
    Testing

---
+++ CMyObject.cpp
@@ -0,0 +1,17 @@
+// CMyObject.cpp : Implementation of CCMyObject
+#include "stdafx.h"
+#include "Foo.h"
+#include "CMyObject.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CCMyObject
+
+
+STDMETHODIMP CCMyObject::Connect()
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState())
+
+	// TODO: Add your implementation code here
+
+	return S_OK;
+}
