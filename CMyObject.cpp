a9703a6db09089ba6df1d1a9fccc768e26aa64ee
john144 <john144@gmail.com>
2014-02-13 16:08:50 -0600

    Added two lines
    Testing two changed

--- CMyObject.cpp
+++ CMyObject.cpp
@@ -12,6 +12,12 @@ STDMETHODIMP CCMyObject::Connect()
 	AFX_MANAGE_STATE(AfxGetStaticModuleState())
 
 	// TODO: Add your implementation code here
+	
+	// 2/13/2014 4:03:29 PM
+	int a = 1 + 2:
+
+	// 2/13/2014 4:06:45 PM
+	int b = a + 1:
 
 	return S_OK;
 }
